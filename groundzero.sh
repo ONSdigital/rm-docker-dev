631901ab2bd473b74df644cc11e35102dcf97133
KieranWardle <kieran.wardle@ons.gov.uk>
2017-05-19 10:36:02 +0100

    Renamed containers and fixed docker-compose file

--- groundzero.sh
+++ groundzero.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  docker stop rmdockerdev_ons-postgres_1
-  docker rm rmdockerdev_ons-postgres_1
+  docker stop postgres
+  docker rm postgres
   docker-compose up -d
   docker ps
 elif [ "$1" == "case" ]; then
-  docker exec rmdockerdev_ons-postgres_1 psql -U postgres -f /docker-entrypoint-initdb.d/case_groundzero.sql
+  docker exec postgres psql -U postgres -f /docker-entrypoint-initdb.d/case_groundzero.sql
 elif [ "$1" = "action" ]; then
-  docker exec rmdockerdev_ons-postgres_1 psql -U postgres -f /docker-entrypoint-initdb.d/action_groundzero.sql
+  docker exec postgres psql -U postgres -f /docker-entrypoint-initdb.d/action_groundzero.sql
 elif [ "$1" = "actionexp" ]; then
-  docker exec rmdockerdev_ons-postgres_1 psql -U postgres -f /docker-entrypoint-initdb.d/actionexp_groundzero.sql
+  docker exec postgres psql -U postgres -f /docker-entrypoint-initdb.d/actionexp_groundzero.sql
 elif [ "$1" = "iac" ]; then
-  docker exec rmdockerdev_ons-postgres_1 psql -U postgres -f /docker-entrypoint-initdb.d/iac_groundzero.sql
+  docker exec postgres psql -U postgres -f /docker-entrypoint-initdb.d/iac_groundzero.sql
 elif [ "$1" = "sample" ]; then
-  docker exec rmdockerdev_ons-postgres_1 psql -U postgres -f /docker-entrypoint-initdb.d/sample_groundzero.sql
+  docker exec postgres psql -U postgres -f /docker-entrypoint-initdb.d/sample_groundzero.sql
+elif [ "$1" = "collex" ]; then
+  docker exec postgres psql -U postgres -f /docker-entrypoint-initdb.d/collex_groundzero.sql
 else
   echo "Invalid argument."
 fi
