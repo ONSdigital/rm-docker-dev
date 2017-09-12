5945fa4db5b59a7b39d622373887ff5cc3ea7ff0
KieranWardle <kieran.wardle@ons.gov.uk>
2017-09-12 12:30:33 +0100

    Fixed test data for collection_instrument

--- ras-compose.sh
+++ ras-compose.sh
@@ -3,7 +3,7 @@
 ###
 # Creates ras-party and ras-collection-instrument containers and populates the database with test data for them
 ###
-
+docker-compose -f docker-compose-ras.yml down
 docker-compose -f docker-compose-ras.yml up -d
-sleep 15 
+sleep 20 
 docker exec postgres psql -U postgres -d postgres -f collection_instrument_test_data.sql
