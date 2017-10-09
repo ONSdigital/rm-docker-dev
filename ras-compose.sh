fff7ea7882852b6890f3ebecd017e564f57c5a54
Joseph Walton <jwgwalton@gmail.com>
2017-10-09 15:27:54 +0100

    start all services in ras-compose

--- ras-compose.sh
+++ ras-compose.sh
@@ -2,39 +2,36 @@
 
 # stop all running containers
 down() {
+ docker-compose -f docker-compose-services.yml down
  docker-compose -f docker-compose-ras.yml down
  docker-compose -f docker-compose-dev_env.yml down
 }
 
 pull() {
-docker-compose -f docker-compose-dev_env.yml pull
-docker-compose -f docker-compose-ras.yml pull
+ docker-compose -f docker-compose-dev_env.yml pull
+ docker-compose -f docker-compose-ras.yml pull
+ docker-compose -f docker-compose-services.yml pull
 }
 
 up() {
-docker-compose -f docker-compose-dev_env.yml up -d
-docker-compose -f docker-compose-ras.yml up -d
+ docker-compose -f docker-compose-dev_env.yml up -d
+ docker-compose -f docker-compose-ras.yml up -d
+ docker-compose -f docker-compose-services.yml up -d
 }
 
 usage() {
-echo "Script to run up required containers for RAS, use flags:- clean: pull containers and run, up rebuild and restart all containers, down: stop all running containers"
+echo "Script to run up RM CONTAINERS, use flags:- clean: pull containers and run, up rebuild and restart all containers, down: stop all running containers"
 }
 
-populate() {
-sleep 10
-docker exec postgres psql -U postgres -d postgres -f collection_instrument_test_data.sql
-}
 case "$1" in
  up) 
    down
    up
-   populate
    ;;
  clean)
    down 
    pull
    up
-   populate
    ;;
  down)
    down
